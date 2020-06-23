d118521359beff1ca09c39902fb657341e58db08
XProjects B.Hummel <infos@x-projects.de>
2020-06-23 21:52:38 +0200

    UpdateStartupScript

--- src/main/resources/data/startXHomeautomation.sh
+++ src/main/resources/data/startXHomeautomation.sh
@@ -2,4 +2,4 @@
 cd /home/pi/xhomeautomation
 mount /dev/sda1 /media/xhomeautomationusb/
 sudo mount | grep sda1
-java -Xdebug -Xrunjdwp:transport=dt_socket,address=5001,server=y,suspend=n -jar XHomeautomation-Service-Deploy.jar /home/pi/xhomeautomation/homeautomation.properties --spring.profiles.active=prod --spring.datasource.url=jdbc:h2:file:/media/xhomeautomationusb/xhomeautomationdb_prod
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5001 -jar Alpdesk-Automationservice-Deploy.jar /home/pi/xhomeautomation/homeautomation.properties --spring.profiles.active=prod --spring.datasource.url=jdbc:h2:file:/media/xhomeautomationusb/xhomeautomationdb_prod
