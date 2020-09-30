0b74ab0d4560f1c730d4d72602d320b2257f50ea
XProjects B.Hummel <infos@x-projects.de>
2020-09-30 18:38:50 +0200

    Update Dependencies and Properties and Version

--- src/main/java/x/main/XHomeautomationMain.java
+++ src/main/java/x/main/XHomeautomationMain.java
@@ -29,7 +29,7 @@ public class XHomeautomationMain implements ApplicationRunner {
 
   Logger logger = LoggerFactory.getLogger(XHomeautomationMain.class);
 
-  public static final String VERSION = "2.2.1";
+  public static final String VERSION = "2.2.2";
 
   @Autowired
   RetainProcess retainProcess;
